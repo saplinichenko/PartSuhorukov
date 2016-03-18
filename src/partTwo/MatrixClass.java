aea625345dfc6c678986919f72606cd0001b32b2
alexlacky <alexlacky@gmail.com>
2016-03-18 23:37:30 +0600

    В данном задании не не доделано инвертирование матрицы и вычисление ранга матрицы. Остальное все реализовано.

--- src/partTwo/MatrixClass.java
+++ src/partTwo/MatrixClass.java
@@ -5,13 +5,13 @@ import java.util.Random;
 public class MatrixClass {
 	int n = 0;
 	int m = 0;
-	final Random random  = new Random();
-	private int[][]  matrixArr;
+	final Random random = new Random();
+	private int[][] matrixArr;
 
 	MatrixClass() {
 		n = 3;
 		m = 3;
-		matrixArr  = fillingMatrix(n, m);
+		matrixArr = fillingMatrix(n, m);
 	}
 
 	MatrixClass(int n, int m) {
@@ -34,7 +34,7 @@ public class MatrixClass {
 	}
 
 	// Метод вывода на экран содержимого матрицы
-	public void printMatrix(MatrixClass tmpMatrix) {
+	public static void printMatrix(MatrixClass tmpMatrix) {
 		System.out.println("==================");
 		for (int i = 0; i < tmpMatrix.matrixArr.length; i++) {
 			for (int j = 0; j < tmpMatrix.matrixArr[0].length; j++) {
@@ -48,14 +48,15 @@ public class MatrixClass {
 	// сравнение двух матриц. На вход метода отправляем объект.
 	public int compare(MatrixClass tmpMatrix) {
 		if (this.matrixArr.length == tmpMatrix.matrixArr.length
-				&& this.matrixArr [0].length == tmpMatrix.matrixArr [0].length) {
+				&& this.matrixArr[0].length == tmpMatrix.matrixArr[0].length) {
 			return 1;// Если матрицы равны возвращаем 1
-			
+
 		} else {
 			return 0;// Если не равны - возвращаем 0;
 		}
 	}
-	//Сумма матриц. Возможно когда размеры матриц равны
+
+	// Сумма матриц. Возможна когда размеры матриц равны
 	public MatrixClass sumMatrix(MatrixClass matrixSecond) {
 		MatrixClass result = new MatrixClass();
 		if (this.compare(matrixSecond) == 1) {
@@ -69,7 +70,8 @@ public class MatrixClass {
 			return null;
 		}
 	}
-	//Разность матриц. Возможно когда размеры матриц равны
+
+	// Разность матриц. Возможно когда размеры матриц равны
 	public MatrixClass diffMatrix(MatrixClass matrixSecond) {
 		MatrixClass result = new MatrixClass();
 		if (this.compare(matrixSecond) == 1) {
@@ -97,6 +99,77 @@ public class MatrixClass {
 		return result;
 	}
 
+	// Определитель матрицы
+	// Используем метод разложения по строке или столбцу
+	// Так как в условии задачи дефолтная матрица 3х3, то допольнительные
+	// условия проверки не стал вставлять
+
+	public static int determinantMatrix(MatrixClass otherMtx) {
+		int determinant = 9999999; // на всякий случай, для проверки
+		int tmpDet1 = 0;
+		int tmpDet2 = 0;
+		int tmpDet3 = 0;
+		int size = otherMtx.matrixArr.length;
+		int[][] tmpArr = new int[size][size];
+		tmpArr = otherMtx.matrixArr;
+
+		for (int i = 0; i < tmpArr.length; i++) {
+			for (int j = 0; j < tmpArr.length; j++) {
+
+				if (i == 0 && j == 0) {
+					tmpDet1 = (int) Math.pow(-1.0, 2.0) * (tmpArr[1][1] * tmpArr[2][2] - tmpArr[2][1] * tmpArr[1][2]);
+				}
+				if (i == 1 && j == 0) {
+					tmpDet2 = (int) Math.pow(-1.0, 3.0) * (tmpArr[0][1] * tmpArr[2][2] - tmpArr[2][1] * tmpArr[2][1]);
+				}
+				if (i == 2 && j == 0) {
+					tmpDet3 = (int) Math.pow(-1.0, 4.0) * (tmpArr[0][1] * tmpArr[1][2] - tmpArr[1][1] * tmpArr[0][2]);
+				}
+
+			}
+
+		}
+		determinant = tmpDet1 + tmpDet2 + tmpDet3;
+		return determinant;
+
+	}
+
+	// метод инвертирования квадратной матрицы второго порядка. Статический. Без
+	// параметров
+	public void invertMatrix() {
+		MatrixClass tmpMatrix = new MatrixClass(2, 2);
+		MatrixClass iMatrix = new MatrixClass(2, 2);
+
+		if (MatrixClass.determinantMatrix(tmpMatrix) != 0) {
+			iMatrix.matrixArr[0][0] = tmpMatrix.matrixArr[1][1];
+			iMatrix.matrixArr[1][1] = tmpMatrix.matrixArr[0][0];
+			iMatrix.matrixArr[1][0] = tmpMatrix.matrixArr[0][1];
+			iMatrix.matrixArr[0][1] = tmpMatrix.matrixArr[1][0];
+
+		} else {
+
+			System.out.println("Инвертирование невозможно. Определитель матрицы равено 0");
+		}
+
+	}
+
+	// метод возврата единичной матрицы третьего порядка
+	// через статичное поле и метод
+	public static MatrixClass identityMatrix( ) {
+		MatrixClass identMatrix = new MatrixClass(3,3);
+		for (int i = 0; i < identMatrix.matrixArr.length; i++) {
+			for (int j = 0; j < identMatrix.matrixArr.length; j++) {
+				if (i == j) {
+					identMatrix.matrixArr[i][j] = 1;
+				}else {
+					identMatrix.matrixArr[i][j] = 0;
+				}
+
+			}
+		}
+		return identMatrix;
+	}
+
 	public int getVerticalSize() {
 		return this.matrixArr.length;
 	}
@@ -104,4 +177,5 @@ public class MatrixClass {
 	public int getHorizontalSize() {
 		return this.matrixArr[0].length;
 	}
+
 }
--- src/partTwo/MatrixTest.java
+++ src/partTwo/MatrixTest.java
@@ -6,33 +6,39 @@ public class MatrixTest {
 		MatrixClass matrix1 = new MatrixClass();
 		MatrixClass matrix2 = new MatrixClass(3, 3);
 		MatrixClass summ;
-		MatrixClass diff; 
+		MatrixClass diff;
 		MatrixClass multNum;
-		int matrixMultiplier = 6; //множитель матрицы (когда умножаем на число)
-		
+		MatrixClass identutyMatrix1= MatrixClass.identityMatrix();
+		int matrixMultiplier = 6; // множитель матрицы (когда умножаем на число)
+
 		System.out.println("Вертикальный размер умолчательной матрицы: " + matrix1.getVerticalSize());
 		System.out.println("Горизонтальный размер умолчательной матрицы: " + matrix1.getHorizontalSize());
-		matrix1.printMatrix(matrix1);
+		MatrixClass.printMatrix(matrix1);
 
 		System.out.println("Вертикальный размер вспомогательной матрицы: " + matrix2.getVerticalSize());
 		System.out.println("Горизонтальный размер вспомогательной матрицы: " + matrix2.getHorizontalSize());
-		matrix2.printMatrix(matrix2);
+		MatrixClass.printMatrix(matrix2);
 
-		
+
 		summ = matrix1.sumMatrix(matrix2);
 		diff = matrix1.diffMatrix(matrix2);
-		
+
 		if (summ != null && diff != null) {
 			System.out.println("Сумма матриц равна: ");
-			matrix2.printMatrix(summ);
+			MatrixClass.printMatrix(summ);
 			System.out.println("Разность матриц равна: ");
-			matrix2.printMatrix(diff);
+			MatrixClass.printMatrix(diff);
 		} else {
 			System.out.println("Размеры матриц не совпадают. Сложение и разность невозможны");
 		}
-		
+
 		multNum = matrix1.matrixMultNumber(matrix1, matrixMultiplier);
 		System.out.println("Умножение матрицы на число: ");
-		matrix1.printMatrix(multNum);
+		MatrixClass.printMatrix(multNum);
+		System.out.println("\n ===== \n Определитель матрицы равен: " + MatrixClass.determinantMatrix(matrix1));
+		
+		System.out.println("Единичная матрица: ");
+		MatrixClass.printMatrix(identutyMatrix1);
+		
 	}
 }
