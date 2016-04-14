84d2b25100a2a8d0dbe537ebdc33d5313803c00c
alexlacky <alexlacky@gmail.com>
2016-04-14 23:37:19 +0600

    feat: Задача номер 1 по классам коллекциям

---
+++ src/classColl/WorkingHours.java
@@ -0,0 +1,26 @@
+package classColl;
+
+public class WorkingHours {  
+	long timeStart;
+	long timeEnd;
+	long triceTimeIn;
+	long triceTimeOut;
+	String nameCollection;
+
+	WorkingHours(long start, long end) {
+		this.timeStart = start;
+		this.timeEnd = end;
+		this.triceTimeIn = 0;
+		this.triceTimeOut = 0;
+	}
+
+	public void triceTimeIn() {
+		triceTimeIn = timeEnd - timeStart;
+	}
+
+	public void triceTimeOut() {
+
+		triceTimeOut = timeEnd - timeStart;
+
+	}
+}
