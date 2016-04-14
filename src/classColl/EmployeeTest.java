47adeddfe54dc2f0c38500fac082977c6180075f
alexlacky <alexlacky@gmail.com>
2016-04-14 22:35:14 +0600

    feat: Доделано первое задание по коллекциям "classColl" (добавлен функционал измерения времени работы)

--- src/classColl/EmployeeTest.java
+++ src/classColl/EmployeeTest.java
@@ -1,5 +1,5 @@
 package classColl;
- 
+
 import java.util.*;
 
 public class EmployeeTest {
@@ -9,20 +9,34 @@ public class EmployeeTest {
 		HashSet<Employee> setEmployee = new HashSet<Employee>(); // Множество
 		ArrayDeque<Employee> queueEmployee = new ArrayDeque<Employee>(); // Очередь
 
-		fillEmployee(arrEmployee, 100); // Заполняем список 100 работников
-		fillEmployee(setEmployee, 50); // Заполняем Множество 50 работников
-		fillEmployee(queueEmployee, 12);// Заполняем очередь 12 работников
+		// Объекты для хранения времени выполнения методов (заполнения и вывода)
+		WorkingHours arrTime = new WorkingHours(0, 0);
+		WorkingHours setTime = new WorkingHours(0, 0);
+		WorkingHours queueTime = new WorkingHours(0, 0);
 
-		printEmployee(arrEmployee, 5);
+		fillEmployee(arrEmployee, 100, arrTime); // Заполняем список 100
+		fillEmployee(setEmployee, 100, setTime); // Заполняем Множество 
+		fillEmployee(queueEmployee, 100, queueTime);// Заполняем очередь 
+
+		printEmployee(arrEmployee, 5, arrTime); // Выводим список 
 		System.err.println("-----------------------------");
-		printEmployee(setEmployee, 2);
+		printEmployee(setEmployee, 2, setTime); // Выводим множество 
 		System.err.println("-----------------------------");
-		printEmployee(queueEmployee, 0);
+		printEmployee(queueEmployee, 0, queueTime); // Выводим очередь
+
+		//Вывод времени работы методов
+		timeEmployee(arrTime);
+		timeEmployee(setTime);
+		timeEmployee(queueTime);
 
 	}
 
 	// Ф-я вывода коллекции
-	public static void printEmployee(Collection<Employee> employees, int workAge) {
+	public static void printEmployee(Collection<Employee> employees, int workAge, WorkingHours tmpObjTime) {
+
+		// Время начала работы метода
+		long timeStart = System.nanoTime();
+
 		Iterator<Employee> iterator = employees.iterator();
 		while (iterator.hasNext()) {
 			Employee tmpObjIter = iterator.next();
@@ -31,10 +45,21 @@ public class EmployeeTest {
 
 		}
 
+		// время окончания работы метода
+		long timeEnd = System.nanoTime();
+		tmpObjTime.timeStart = timeStart;
+		tmpObjTime.timeEnd = timeEnd;
+		tmpObjTime.triceTimeOut();
+
 	}
 
 	// Ф-я заполнения коллекции
-	public static void fillEmployee(Collection<Employee> employees, int amountWorkers) {
+	public static void fillEmployee(Collection<Employee> employees, int amountWorkers, WorkingHours tmpObjTime) {
+		// имя типа коллекции
+		String nameCollection = employees.getClass().getName().substring(10);
+
+		// Время начала работы метода
+		long timeStart = System.nanoTime();
 		String[] f = { "Иванов", "Петров", "Сидоров", "Маслов" };
 		String[] i = { "Сергей", "Иван", "Дмитрий", "Пётр" };
 		String[] o = { "Михайлович", "Петрович", "Александрович", "Владимирович" };
@@ -47,6 +72,19 @@ public class EmployeeTest {
 			tmpWorkAge = (int) (Math.random() * 8 + 1);
 			employees.add(new Employee(name, tmpWorkAge));
 		}
+		// время окончания работы метода
+		long timeEnd = System.nanoTime();
+		tmpObjTime.nameCollection = nameCollection;
+		tmpObjTime.timeStart = timeStart;
+		tmpObjTime.timeEnd = timeEnd;
+		tmpObjTime.triceTimeIn();
 	}
 
+	// Раситываем время работы ввода и вывода
+	public static void timeEmployee(WorkingHours tmp) {
+		System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+		System.out.println(tmp.nameCollection + ".\nВремя заполнения (наносек): " + tmp.triceTimeIn
+				+ ". Время вывода (наносек): " + tmp.triceTimeOut);
+
+	}
 }
