d688e05c59dd4fd0452aa5be44b4b1324989587d
alexlacky <alexlacky@gmail.com>
2016-04-14 23:14:41 +0600

    feat: Задача по коллекциям

---
+++ src/class1Collec/Employee.java
@@ -0,0 +1,13 @@
+package class1Collec;
+ 
+public class Employee { 
+	String name;
+	int workAge;
+ 
+	Employee(String name, int workAge) {
+		this.name = name;
+		this.workAge = workAge;
+	}
+	
+
+}
