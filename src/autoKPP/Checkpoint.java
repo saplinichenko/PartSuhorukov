9f377077febf9fce6b3f475502f283d6ee3d7fd7
alexlacky <alexlacky@gmail.com>
2016-04-27 00:23:38 +0600

    feat: в проект autoKPP добавлена многопоточность

--- src/autoKPP/Checkpoint.java
+++ src/autoKPP/Checkpoint.java
@@ -1,8 +1,8 @@
 package autoKPP;
- 
+
 import autoKPP.AutoKPPTest.SpecTransport;
 
-public class Checkpoint {
+public class Checkpoint extends Thread {
 	int fareAll; // общая стоимость
 	String fareName; // Наименование марки автомобиля
 	int farePassenger = 1000; // пошлина на проезд легковой авто
@@ -16,6 +16,10 @@ public class Checkpoint {
 	double height; // высота
 	SpecTransport isSpec2; // является ли авто спец.транспортом
 	Object tmpCar; // временный объект
+
+	Checkpoint() {
+
+	}
 
 	Checkpoint(FreightCar fCar) {
 		tmpCar = (FreightCar) tmpCar;
@@ -48,77 +52,81 @@ public class Checkpoint {
 		if (currentSpeed >= 80.0 && currentSpeed <= 180.0) {
 			priceFine = 255;
 		} else if (currentSpeed >= 180.0) {
-			System.out.println("ВНИМАНИЕ!!! Нарушение скоростного порядка! Всем патрулям: марка машины: " + fareName);
+			System.out.println(
+					"ВНИМАНИЕ!!! Нарушение скоростного порядка! Всем патрулям: марка машины: " + fareName );
 		}
 	}
 
-	public void printInfo(int tmpFareAll) {
-		System.out.println("Марка частного легкового автомобиля: " + fareName);
-		System.out.println("Стоимость проезда = " + tmpFareAll + " рублей. Где: ");
-		System.out.println(farePassenger + " руб. - пошлина за въезд легкового автомобиля");
-		System.out.println(priceFine + " руб. - штраф за превышение скорости");
+	@Override
+	public void run() {
+		try {
+			if (this.isSpec2 != null) {
+				switch (this.isSpec2) { // Проверяем на спец.транспорт
+				case AMBULANCE:
+					System.out.println("Марка машины: " + fareName);
+					System.out.println("Скорая помощь. Проезд открыт");
+					break;
+				case FIRESUPPORT:
+					System.out.println("Марка машины: " + fareName);
+					System.out.println("Пожарная служба. Проезд открыт");
+					break;
+				case VEHICLEINSPECTION:
+					System.out.println("Марка машины: " + fareName);
+					System.out.println("Автоинспекция. Проезд открыт");
+					break;
+				}
+			} else if (this.tmpCar instanceof PassengerCar) {
+				speedLimit(); // проверяем скорость стоимость проезда легкового + штрав
+				fareAll = farePassenger + priceFine;
 
-	}
+				Runnable r = new PrintInfoPassengerThread(fareAll, fareName, farePassenger, priceFine);
+				Thread t = new Thread(r);
+				t.start();
+				t.join();
 
-	public void printInfo(int tmpFareAll, int tmpFreight, int priceFine, int tmpTrailer, int tmpCapacity) {
-		System.out.println("Марка частного грузового автомобиля: " + fareName);
-		System.out.println("Стоимость проезда = " + tmpFareAll + " рублей. Где: ");
-		System.out.println(tmpFreight + " руб. - пошлина за въезд легкового автомобиля");
-		System.out.println(tmpTrailer + " руб. - пошлина за прицеп");
-		System.out.println(tmpCapacity + " руб. - пошлина за превышение веса (более 10 тонн)");
-		System.out.println(priceFine + " руб. - штраф за превышение скорости");
+			} else if (this.tmpCar instanceof FreightCar) {
+				speedLimit();// проверяем скорость
 
-	}
+				// стоимость проезда грузового + штрав
+				if (height > 4) {
+					System.out.println("Проезд невозможен!!!");
 
-	public void calculateBill() {
-		if (this.isSpec2 != null) {
-			switch (this.isSpec2) { // Проверяем на спец.транспорт
-			case AMBULANCE:
-				System.out.println("Марка машины: " + fareName);
-				System.out.println("Скорая помощь. Проезд открыт");
-				break;
-			case FIRESUPPORT:
-				System.out.println("Марка машины: " + fareName);
-				System.out.println("Пожарная служба. Проезд открыт");
-				break;
-			case VEHICLEINSPECTION:
-				System.out.println("Марка машины: " + fareName);
-				System.out.println("Автоинспекция. Проезд открыт");
-				break;
-			}
-		} else if (this.tmpCar instanceof PassengerCar) {
-			speedLimit(); // проверяем скорость
-			// стоимость проезда легкового + штрав
-			fareAll = farePassenger + priceFine;
-			printInfo(fareAll);
+				} else if (height <= 4 && this.hasTrailer && this.capacity > 10.0) {
+					fareAll = fareFreight + priceFine + priceTrailer + priceCapacity;
 
-		} else if (this.tmpCar instanceof FreightCar) {
-			speedLimit();// проверяем скорость
+					Runnable r = new PrintInfoFreightThread(fareAll, fareName, fareFreight, priceFine, priceTrailer,
+							priceCapacity);
+					Thread t1 = new Thread(r);
+					t1.start();
+					t1.join();
 
-			// стоимость проезда грузового + штрав
-			if (height > 4) {
-				System.out.println("Проезд невозможен!!!");
+				} else if (height <= 4 && this.hasTrailer && this.capacity < 10.0) {
+					fareAll = fareFreight + priceFine + priceTrailer + 0;
 
-			} else if (height <= 4 && this.hasTrailer && this.capacity > 10.0) {
-				fareAll = fareFreight + priceFine + priceTrailer + priceCapacity;
-				printInfo(fareAll, fareFreight, priceFine, priceTrailer, priceCapacity);
-
-			} else if (height <= 4 && this.hasTrailer && this.capacity < 10.0) {
-				fareAll = fareFreight + priceFine + priceTrailer + 0;
-				printInfo(fareAll, fareFreight, priceFine, priceTrailer, 0);
+					Runnable r = new PrintInfoFreightThread(fareAll, fareName, fareFreight, priceFine, priceTrailer, 0);
+					Thread t2 = new Thread(r);
+					t2.start();
+					t2.join();
+				}
 
 			} else if (height <= 4 && !this.hasTrailer && this.capacity > 10.0) {
 				fareAll = fareFreight + priceFine + 0 + priceCapacity;
-				printInfo(fareAll, fareFreight, priceFine, 0, priceCapacity);
 
-			} else if (height <= 4 && !this.hasTrailer && this.capacity < 10.0) {
+				Runnable r = new PrintInfoFreightThread(fareAll, fareName, fareFreight, priceFine, 0, priceCapacity);
+				Thread t3 = new Thread(r);
+				t3.start();
+				t3.join();
+
+			} else if (height <= 4.0 && !this.hasTrailer && this.capacity < 10.0) {
 				fareAll = fareFreight + priceFine + 0 + 0;
-				printInfo(fareAll, fareFreight, priceFine, 0, 0);
+
+				Runnable r = new PrintInfoFreightThread(fareAll, fareName, fareFreight, priceFine, 0, 0);
+				Thread t4 = new Thread(r);
+				t4.start();
+				t4.join();
 
 			}
-
+		} catch (InterruptedException e) {
 		}
-
 	}
-
 }
